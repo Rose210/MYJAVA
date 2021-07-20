e6d7a7b31152691f1401516253018cec782cc0de
Rose210 <84668996+Rose210@users.noreply.github.com>
2021-07-20 21:26:18 +0530

    Add files via upload
    Prime Number

---
+++ PrimeNumber.java
@@ -0,0 +1,28 @@
+package july2021;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner (System.in);
+		 System.out.println("Enter a number");
+		 int n = sc.nextInt();
+		 boolean isPrime = true;
+		 for(int i=2;i<n;i++){
+			 if(n%i==0){
+				 isPrime=false;
+				 break;
+			 }
+		 
+		 }
+		 if(isPrime){
+			 System.out.println(+n+" is a Prime number");
+		 }
+		 else{
+			 System.out.println(+n+" is not a Prime number");
+		 }
+
+	}
+
+}
